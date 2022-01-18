4363f43dc8d4c9ebd57372799ceeb5a910bf30c3
Rudy Meijer <rudy.meijer@gmail.com>
2022-01-18 14:20:39 +0100

    Add project files.

---
+++ BlazorServerSignalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServerSignalRApp.Server.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorServerSignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
